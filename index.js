ff219080cd8ba2e146685a9fc38984bb3fb5ec82
sukkyuknowre <scarfunk@naver.com>
2019-05-16 15:06:04 +0900

    add gql

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 var express = require('express');
 var cors = require('cors');
 var app = express();
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+
+var root = { hello: () => 'Hello world!' };
 
 var cors = require('cors');
 var corsOptions = {
-    origin: '*',
+    origin: 'http://10.210.1.77:8020',
     credentials: true
 };
 
 app.use(cors(corsOptions));
 
-app.get('/test', function (req, res, next) {
+app.use('/graphql', graphqlHTTP((request, res) => {
     res.cookie('skoh', 'hi skoh', { maxAge: 900000, httpOnly: true });
-    res.json({ msg: 'GET call This is CORS-enabled for all origins!' })
-});
+    return {
+        schema: schema,
+        rootValue: root,
+        context: { startTime: Date.now() },
+        graphiql: true,
+    };
+}));
 
-app.post('/test', function (req, res, next) {
-    res.cookie('skoh', 'hi skoh', { maxAge: 900000, httpOnly: true });
-    res.json({ msg: 'POST call res!' })
-});
+// app.get('/test', function (req, res, next) {
+//     res.cookie('skoh', 'hi skoh', { maxAge: 900000, httpOnly: true });
+//     res.json({ msg: 'GET call This is CORS-enabled for all origins!' })
+// });
 
-
-app.listen(9999, function () {
+app.listen(8080, function () {
     console.log('CORS-enabled web server listening on port 9999')
 });
